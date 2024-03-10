42ebe49e94b95e219754b16b975d1af21d867eda
Cutecookies <cat.O1204@yandex.ru>
2024-03-10 19:01:12 +0300

    functions for testing

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -93,4 +93,15 @@ position getMinValuePos(matrix m);
 // возвращает позицию максимального элемента матрицы m.
 position getMaxValuePos(matrix m);
 
+// возвращает матрицу размера nRows на nCols,
+// построенную из элементов массива a.
+matrix createMatrixFromArray(const int *a, int nRows,
+                             int nCols);
+
+// возвращает указатель на нулевую матрицу массива из
+// nMatrices матриц, размещенных в динамической памяти,
+// построенных из элементов массива a.
+matrix *createArrayOfMatrixFromArray(const int *values, int nMatrices,
+                                     int nRows, int nCols);
+
 #endif //LABORATORIESWORKS_MATRIX_H
