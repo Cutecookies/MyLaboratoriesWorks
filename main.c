517d3804e3abcb900e76d1ae09a284a00087f1fd
Cutecookies <cat.O1204@yandex.ru>
2024-04-07 01:47:33 +0300

    search functions

--- main.c
+++ main.c
@@ -14,9 +14,79 @@ void test_strlen_manyZeroElements() {
     assert(strlen_(word) == 5);
 }
 
+// есть элемент
+void test_ind_isElement() {
+    char word[7] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    assert(find(word, word + 3, 'e') == word + 1);
+}
+
+// нет элемента
+void test_ind_isNoElement() {
+    char word[7] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    assert(find(word, word + 3, 'k') == word + 3);
+}
+
+// есть не пробельный символ
+void test_findNonSpace_isNonSpace() {
+    char word[7] = {' ', ' ', ' ', 'k', 'o', '\0'};
+    assert(findNonSpace(word) == word + 3);
+}
+
+// нет не пробельного символа
+void test_findNonSpace_noNonSpace() {
+    char word[7] = {' ', ' ', ' ', '\0'};
+    assert(findNonSpace(word) == word + 3);
+}
+
+// есть пробельный символ
+void test_findSpace_isSpace() {
+    char word[7] = {'g', 'h', ' ', 't', ' ', '\0'};
+    assert(findSpace(word) == word + 2);
+}
+
+// нет пробельного символа
+void test_findSpace_noSpace() {
+    char word[7] = {'k', 'w', 'a', '\0'};
+    assert(findSpace(word) == word + 3);
+}
+
+// есть не пробельный символ
+void test_findNonSpaceReverse_isNonSpace() {
+    char word[7] = {'j', ' ', ' ', 'l', ' ', '\0'};
+    assert(findNonSpaceReverse(word + strlen_(word) - 1, word - 1) == word + 3);
+}
+
+// нет не пробельного символа
+void test_findNonSpaceReverse_noNonSpace() {
+    char word[7] = {' ', ' ', ' ', '\0'};
+    assert(findNonSpaceReverse(word + strlen_(word) - 1, word - 1) == word - 1);
+}
+
+// есть пробельный символ
+void test_findSpaceReverse_isNonSpace() {
+    char word[7] = {'j', ' ', ' ', 'h', 'p', '\0'};
+    assert(findSpaceReverse(word + strlen_(word) - 1, word - 1) == word + 2);
+}
+
+// нет пробельного символа
+void test_findSpaceReverse_noNonSpace() {
+    char word[7] = {'A', 'B', 'C', '\0'};
+    assert(findSpaceReverse(word + strlen_(word) - 1, word - 1) == word - 1);
+}
+
 void test() {
     test_strlen_oneZeroElement();
     test_strlen_manyZeroElements();
+    test_ind_isElement();
+    test_ind_isNoElement();
+    test_findNonSpace_isNonSpace();
+    test_findNonSpace_noNonSpace();
+    test_findSpace_isSpace();
+    test_findSpace_noSpace();
+    test_findNonSpaceReverse_isNonSpace();
+    test_findNonSpaceReverse_noNonSpace();
+    test_findSpaceReverse_isNonSpace();
+    test_findSpaceReverse_noNonSpace();
 }
 
 int main() {
